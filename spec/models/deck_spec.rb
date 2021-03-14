ab57f56b2a521c35077e89388646d8134d86e772
Daniel Mulitauopele <daniel.mulitauopele@logrhythm.com>
2021-03-14 13:46:37 -0600

    Adds deck class

---
+++ spec/models/deck_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+describe Deck, type: :model do
+    describe 'validations' do
+        it {should validate_presence_of(:name)}
+        it {should validate_presence_of(:type)}
+        it {should validate_presence_of(:count)}
+    end
+
+    describe 'relationships' do
+        it {should have_many :deck_cards}
+        it {should have_many :cards}
+    end
+end
